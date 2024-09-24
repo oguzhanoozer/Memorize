492144994054d0f38104f9b0bdedc8a82dcaed70
oguzhanoozer <oguzoozer@gmail.com>
2024-09-24 12:33:27 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by oguzhan on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
